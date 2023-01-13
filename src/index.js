cbfa756d2204b6fe3c90d2b8bec7298f9c90ad68
Ernest Bieś <ernestbies@gmail.com>
2023-01-13 15:00:54 +0100

    Component lifecycles update.

--- src/index.js
+++ src/index.js
@@ -108,7 +108,7 @@ class DropdownTreeSelect extends Component {
     }
   }
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     this.initNewProps(this.props)
   }
 
@@ -116,7 +116,7 @@ class DropdownTreeSelect extends Component {
     document.removeEventListener('click', this.handleOutsideClick, false)
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     this.initNewProps(nextProps)
   }
 
